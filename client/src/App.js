855eaa30269efab1f35e171ce3f5b8f9f49f7e69
Sunilrath101 <sunilrath.ssr@gmail.com>
2023-01-01 18:27:51 +0530

    env link

--- client/src/App.js
+++ client/src/App.js
@@ -18,8 +18,8 @@ function App() {
 
   const fetchBotResponse = async () => {
     const { data } = await axios.post(
-      // "https://chatgpt-ai-app-od21.onrender.com",
-      "http://localhost:5000",
+      "https://autochat.onrender.com",
+
       { input },
       {
         headers: {
